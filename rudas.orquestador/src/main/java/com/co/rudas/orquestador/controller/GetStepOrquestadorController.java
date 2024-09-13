60f581732f1a55fa62193c7bab9bf6439f3f7aea
AgustdSrOscuro7 <valekrudas1@gmail.com>
2024-09-12 19:30:34 -0500

    parcial

---
+++ rudas.orquestador/src/main/java/com/co/rudas/orquestador/controller/GetStepOrquestadorController.java
@@ -0,0 +1,45 @@
+package com.co.rudas.orquestador.controller;
+
+import org.apache.camel.ProducerTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orques/steps")
+public class GetStepOrquestadorController {
+
+    @Autowired
+    private ProducerTemplate producerTemplate;
+
+    @PostMapping("/step-one")
+    public ResponseEntity<String> getStepOne(@RequestBody String requestBody) {
+        String response = producerTemplate.requestBody("direct:getStepOne", requestBody, String.class);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/step-two")
+    public ResponseEntity<String> getStepTwo(@RequestBody String requestBody) {
+
+        String response = producerTemplate.requestBody("direct:getStepTwo", requestBody, String.class);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/step-three")
+    public ResponseEntity<String> getStepThree(@RequestBody String requestBody) {
+
+        String response = producerTemplate.requestBody("direct:getStepThree", requestBody, String.class);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/orchestration")
+    public ResponseEntity<String> startOrchestration(@RequestBody String requestBody) {
+
+        String response = producerTemplate.requestBody("direct:startOrchestration", requestBody, String.class);
+        return ResponseEntity.ok(response);
+    }
+}
+
